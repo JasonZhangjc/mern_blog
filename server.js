a8fecff6afcebad65cd8a82282ac421a9999e3df
JasonZhangjc <jasonzhangjc@outlook.com>
2023-10-21 21:34:10 -0400

    Connect frontend and backend

--- server.js
+++ server.js
@@ -1,26 +1,47 @@
 const express = require('express');
 const app = express();
+const {MongoClient} = require("mongodb");
 const PORT = process.env.PORT || 8000;
 
-const articlesInfo = {
-    "MongoDB": {
-        comments: [],
-    },
-    "Express.js": {
-        comments: [],
-    },
-    "React.js": {
-        comments: [],
-    },
-    "Node.js": {
-        comments: [],
-    },
-};
+
+// const articlesInfo = {
+//     "MongoDB": {
+//         comments: [],
+//     },
+//     "Express.js": {
+//         comments: [],
+//     },
+//     "React.js": {
+//         comments: [],
+//     },
+//     "Node.js": {
+//         comments: [],
+//     },
+// };
 
 // Initialize middleware
 // we used to have to install body parser but now it is built in middleware
 // function of express. If parses incoming JSON payload
 app.use(express.json({ extended: false }));
+
+const withDB = async(operations, res) => {
+    try {
+        const client = await MongoClient.connect('mongodb://127.0.0.1');
+        const db = client.db("mernblog");
+        await operations(db);
+        client.close();
+    } catch (error) {
+        res.status(500).json({message:"Error connecting to database", error});
+    }
+}
+
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async(db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({name: articleName});
+        res.status(200).json(articleInfo);
+    }, res)
+});
 
 // // Just a test route for now
 // app.get('/', (req, res) => res.send("Hello Jason!"));
@@ -30,9 +51,24 @@ app.use(express.json({ extended: false }));
 app.post('/api/articles/:name/add-comments', (req, res) => {
     const {username, text} = req.body;
     const articleName = req.params.name;
-    articlesInfo[articleName].comments.push({username, text});
-    res.status(200).send(articlesInfo[articleName]);
+    withDB(async(db) => {
+        const articleInfo = await db
+            .collection('articles')
+            .findOne({name: articleName});
+        await db.collection('articles').updateOne({name: articleName}, {
+            $set : {
+                comments: articleInfo.comments.concat({username, text}),
+            },
+        });
+        const updateArticleInfo = await db
+            .collection('articles')
+            .findOne({name: articleName});
+        res.status(200).json(updateArticleInfo);
+    }, res);
 });
 
 app.listen(PORT, () => console.log(`Server started at port ${PORT}`));
+
+
+
 
